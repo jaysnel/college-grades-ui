02c274867878ec39af383a62e4765d8bfea64bb1
jaysnel <jaylan210@gmail.com>
2022-12-25 15:03:43 -0500

    checking wallet in App.tsx instead of only the homepage. Much better UX and avoids having someone visit a separate page to view data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
+import {useEffect, useState} from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Create from './pages/Create';
 import Home from './pages/Home';
+import ConnectTowallet from './components/ConnectTowallet';
+
+declare const window: Window &
+    typeof globalThis & {
+        ethereum: any
+    }
 
 function App() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const {ethereum} = window;
+      if(ethereum) {
+        const accounts = await ethereum.request({method: 'eth_requestAccounts'});
+        if(accounts.length !== 0) {
+          setWalletConnected(true);
+        } else {
+          setErrorMessage('No wallet found. Connect to a wallet');
+        }
+      }
+    } catch (err: any) {
+      console.log(err);
+      setErrorMessage(err.message);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+
+  if(!walletConnected) {
+    return (
+      <>
+          <ConnectTowallet message={errorMessage}/>
+      </>
+    )
+  }
+
   return (
     <div className="App">
     <BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import {useEffect, useState} from 'react'
 import StudentTable from '../components/StudentTable';
 import contractABI from '../utils/contractABI.json';
-import ConnectTowallet from '../components/ConnectTowallet';
 
     declare const window: Window &
     typeof globalThis & {
@@ -17,26 +16,7 @@ import ConnectTowallet from '../components/ConnectTowallet';
 export default function Home() {
     const {ethers} = require('ethers');
     const contractAddress = '0x261d5ADa2C89369E1AfCAA98d52dEb124DD9f0Ff'
-    const [walletConnected, setWalletConnected] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
     const [studentList, setStudentList] = useState<StudentData[]>([]);
-  
-    const checkIfWalletIsConnected = async () => {
-      try {
-        const {ethereum} = window;
-        if(ethereum) {
-          const accounts = await ethereum.request({method: 'eth_requestAccounts'});
-          if(accounts.length !== 0) {
-            setWalletConnected(true);
-          } else {
-            setErrorMessage('No wallet found. Connect to a wallet');
-          }
-        }
-      } catch (err: any) {
-        console.log(err);
-        setErrorMessage(err.message);
-      }
-    }
   
     const getAllStudentData = async () => {
       try {
@@ -65,17 +45,8 @@ export default function Home() {
     }
   
     useEffect(() => {
-      checkIfWalletIsConnected();
       getAllStudentData();
     }, [])
-  
-    if(!walletConnected) {
-      return (
-        <>
-            <ConnectTowallet message={errorMessage}/>
-        </>
-      )
-    }
 
   return (
     <>
