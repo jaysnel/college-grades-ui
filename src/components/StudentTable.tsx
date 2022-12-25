5a75da1f32c301b07e642f6b4028bae685e1cf93
jaysnel <jaylan210@gmail.com>
2022-12-25 13:44:20 -0500

    finally something working! some css is well needed. If only i was a designer

--- src/components/StudentTable.tsx
+++ src/components/StudentTable.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import {StudentData} from '../pages/Home';
 
-export default function StudentTable(props: { studentData: object[]; }) {
+export default function StudentTable(props: { studentData: StudentData[]; }) {
     const {studentData} = props;
     console.log('Student Table: ', studentData)
+
   return (
     <div className=''>
     <h2>STUDENT TABLE</h2>
@@ -10,31 +11,21 @@ export default function StudentTable(props: { studentData: object[]; }) {
         <thead>
             <tr>
             <th className='border-b border-slate-600 font-medium text-slate-200 text-left'>Name</th>
-            <th>Wallet</th>
-            <th>Courses</th>
+            <th className='border-b border-slate-600 font-medium text-slate-200 text-left'>Age</th>
+            <th className='border-b border-slate-600 font-medium text-slate-200 text-left'>Wallet</th>
+            <th className='border-b border-slate-600 font-medium text-slate-200 text-left'>Courses</th>
             </tr>
         </thead>
         <tbody className='bg-slate-800'>
-            {/* {studentData.map((student, idx) => {
+            {studentData.map((student, idx) => {
                 return (
-                    <td>{student.name}</td>
+                    <tr key={idx} className='text-slate-200'>
+                        <td>{student.name}</td>
+                        <td>{student.age}</td>
+                        <td>{student.wallet}</td>
+                    </tr>
                 )
-            })} */}
-            <tr>
-            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-            <td>Malcolm Lockyer</td>
-            <td>1961</td>
-            </tr>
-            <tr>
-            <td>Witchy Woman</td>
-            <td>The Eagles</td>
-            <td>1972</td>
-            </tr>
-            <tr>
-            <td>Shining Star</td>
-            <td>Earth, Wind, and Fire</td>
-            <td>1975</td>
-            </tr>
+            })}
         </tbody>
     </table>
 </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,35 +3,33 @@ import StudentTable from '../components/StudentTable';
 import contractABI from '../utils/contractABI.json';
 import ConnectTowallet from '../components/ConnectTowallet';
 
-declare const window: Window &
-  typeof globalThis & {
-    ethereum: any
-}
+    declare const window: Window &
+    typeof globalThis & {
+        ethereum: any
+    }
 
-interface IntroData {
-    title: string;
-    id: number;
-  }
+    interface IntroData {
+        title: string;
+        id: number;
+    }
 
   
-  const introData: IntroData = {
-    title: "College Grades",
-    id: 0,
-  };
-  
-  type StudentData = {
-    name: string; 
-    age: string; 
-    wallet: string; 
-    courses: number;
-};
+    const introData: IntroData = {
+        title: "College Grades",
+        id: 0,
+    };
+    
+    export interface StudentData {
+        name: string; 
+        age: string; 
+        wallet: string; 
+        courses: number;
+    };
 
 export default function Home() {
     const {ethers} = require('ethers');
     const contractAddress = '0x261d5ADa2C89369E1AfCAA98d52dEb124DD9f0Ff'
     const [walletConnected, setWalletConnected] = useState(false);
-    const [currentAccount, setCurrentAccount] = useState('');
-    const [isError, setIsError] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
     const [studentList, setStudentList] = useState<StudentData[]>([]);
   
@@ -41,10 +39,8 @@ export default function Home() {
         if(ethereum) {
           const accounts = await ethereum.request({method: 'eth_requestAccounts'});
           if(accounts.length !== 0) {
-            setCurrentAccount(accounts[0]);
             setWalletConnected(true);
           } else {
-            setIsError(true)
             setErrorMessage('No wallet found. Connect to a wallet');
           }
         }
@@ -62,22 +58,18 @@ export default function Home() {
           await provider.send("eth_requestAccounts", []);
           const signer = provider.getSigner();
           const contract = new ethers.Contract(contractAddress, contractABI, signer)
-          // console.log(contract)
           const studentCount = await contract.getTotalStudentCount();
-          // console.log(studentCount.toString());
           for(let i = 0; i < studentCount.toString(); i++) {
             let student = await contract.getStudentData(i);
 
-            const studentData = {
+            const studentData: StudentData = {
                 name: student.name,
                 age: student.age.toString(),
                 wallet: student.wallet,
                 courses: student.courses
             }
-            console.log(studentData)
-            // setStudentList([...studentList, studentData])
-            studentList.push(studentData);
-            console.log('Student Data: ', studentList)
+            // console.log('Student', studentData)
+            setStudentList(prevStudentList => [...prevStudentList, studentData])
           }
         }
       } catch (err) {
@@ -97,8 +89,6 @@ export default function Home() {
         </>
       )
     }
-
-    // console.log(contractABI);
 
   return (
     <>
